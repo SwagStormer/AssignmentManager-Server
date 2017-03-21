41241acc70da582c7c2899497f761041f92108d8
SwagStormer <darthryan6265@gmail.comgit>
2017-03-21 17:43:08 -0600

    fiadded period filtering

--- bell/views.py
+++ bell/views.py
@@ -27,7 +27,7 @@ class PeriodViewSet(viewsets.ModelViewSet):
         now = datetime.now().time().replace(9)
         print(now)
         for period in periods:
-            print(period.start_time >= now)
-            print(period.end_time <= now)
+            print(period.start_time <= now)
+            print(period.end_time >= now)
 
         return []
