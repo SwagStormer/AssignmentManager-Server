da5bb91d70d61146deb88902ce5636555b4f843c
SwagStormer <darthryan6265@gmail.comgit>
2017-03-21 17:04:11 -0600

    added admins for bell

--- bell/admin.py
+++ bell/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from bell.models import Schedule, Period, Date
 # Register your models here.
+
+
+class ScheduleAdmin(admin.ModelAdmin):
+    pass
+
+admin.register(Schedule, ScheduleAdmin)
+
+
+class PeriodAdmin(admin.ModelAdmin):
+    pass
+
+admin.register(Period, PeriodAdmin)
+
+
+class DateAdmin(admin.ModelAdmin):
+    pass
+
+admin.register(Date, DateAdmin)
