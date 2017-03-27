2ff618f90259db7cc06d49f907e8fbc7a048a4c0
SwagStormer <darthryan6265@gmail.comgit>
2017-03-26 21:24:31 -0600

    tasks aren't shown if finished

--- assignment/views.py
+++ assignment/views.py
@@ -54,7 +54,7 @@ class TaskViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         if self.request.user.is_authenticated:
             courses = Course.objects.filter(user=self.request.user.id)
-            return Task.objects.filter(course=courses)
+            return Task.objects.filter(course=courses, finished=False)
         else:
             return Task.objects.none()
 
