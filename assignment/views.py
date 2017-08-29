522ceeb675daf08f5a4373ea24ee2594631fe98e
Ryan <ryan@ryansbees.com>
2017-08-29 12:25:28 -0600

    fixed queryset

--- assignment/views.py
+++ assignment/views.py
@@ -12,6 +12,7 @@ from django.shortcuts import get_object_or_404
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
+    permission_classes = [IsAuthenticated, ]
 
     def get_serializer_class(self):
         if self.request.method == "POST":
@@ -20,15 +21,9 @@ class TaskViewSet(viewsets.ModelViewSet):
             return TaskReadSerializer
 
     def get_queryset(self):
-        if self.request.user.is_authenticated:
-            q = self.request.query_params.get
-            if q('course'):
-                courses = Course.objects.filter(user=self.request.user.id, id=q('course'))
-            else:
-                courses = Course.objects.filter(user=self.request.user.id)
-            return Task.objects.filter(course=courses, is_finished=False)
-        else:
-            return Task.objects.none()
+        q = self.request.query_params.get
+        courses = Course.objects.filter(user=self.request.user.id)
+        return Task.objects.filter(course=courses, is_finished=False)
 
 
 class CourseViewSet(viewsets.ModelViewSet):
